0173f95619b84f7e81408a50b359f0f9f91837ec
Long Nguyen <lnguyen30@uco.edu>
2021-05-19 19:29:09 -0500

    update image from edit form

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ import * as Auth from './controller/auth.js'
 import * as ProductPage from './viewpage/product_page.js'
 import * as Home from './viewpage/home_page.js'
 import * as User from './viewpage/user_page.js'
+import * as Edit from './controller/edit_product.js'
 
 
 window.onload = ()=>{
@@ -24,3 +25,4 @@ Auth.addEventListeners();
 ProductPage.addEventListeners();
 Home.addEventListeners();
 User.addEventListeners();
+Edit.addEventListeners();
--- public/controller/edit_product.js
+++ public/controller/edit_product.js
@@ -3,10 +3,31 @@ import * as Util from '../viewpage/util.js'
 import * as Constant from '../model/constant.js'
 import * as Element from '../viewpage/element.js'
 
+let imageFile2Upload
+
+export function addEventListeners(){
+    Element.formEditProduct.imageButton.addEventListener('change', e=>{
+        imageFile2Upload = e.target.files[0]; //grabs the image of the form
+        if(!imageFile2Upload){
+            Element.formEditProduct.imageTag.src = null;
+            Element.formEditProduct.errorImage.innerHTML = 'Image Change Cancelled. Original image will be used';
+            return;
+        }
+             //takes image error mssage away
+             Element.formEditProduct.errorImage.innerHTML = '';
+             //reads the img file uploaded
+             const reader = new FileReader();
+             //loads image src file to tag and previews the pic
+             reader.readAsDataURL(imageFile2Upload);
+             reader.onload = () => Element.formEditProduct.imageTag.src = reader.result
+             
+    })
+}
 
 export async function edit_product(docId){
     let product;
     try{
+        //calls firebase controller function to edit the product by id
         product = await FirebaseController.getProductById(docId)
         if(!product){
             Util.info('getProductById error', 'No product found by id');
@@ -25,6 +46,7 @@ export async function edit_product(docId){
     Element.formEditProduct.form.price.value = product.price;
     Element.formEditProduct.form.summary.value = product.summary;
     Element.formEditProduct.imageTag.src = product.imageURL;
+    Element.formEditProduct.errorImage.innerHTML = ''
 
     Element.modalEditProduct.show();
 
